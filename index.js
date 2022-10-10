a6b582f236e32103f043cf15189717202f3721ea
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2022-10-10 11:50:16 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const {
+  ALLOW_HEADERS = "*",
+  ALLOW_ORIGINS = "*",
+  ALLOW_METHODS = "OPTIONS,GET",
+} = process.env;
+
+exports.handler = (event) => {
+  return {
+    statusCode: 200,
+    body: "OK",
+    headers: {
+      "Access-Control-Allow-Headers": ALLOW_HEADERS,
+      "Access-Control-Allow-Origin": ALLOW_ORIGINS,
+      "Access-Control-Allow-Methods": ALLOW_METHODS,
+    },
+  };
+};
